f7f929885f715afe0e8672ab2fbd9be226fc431d
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-07-10 18:20:36 -0500

    adding queryselectors

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
-// query selectors
-var startGameButton = document.querySelector("#start-button")
-var 
+// quiz query selectors
+var startGameBtn = document.querySelector("#start-button")
+var quizBox = document.querySelector("#quiz-box")
+var quizQuestion = document.querySelector("#question-header")
+var userAnswer = document.querySelector("#user-answer")
+var submitButton = document.querySelector("#submit-answer")
+
+// save game query selectors
+var quizOver = document.querySelector("#quiz-over-box")
+var initialLabel = document.querySelector("#initialsLabel")
+var quizOver = document.querySelector("#start-button")
+
+// button listeners
+startGameBtn.addEventListener
