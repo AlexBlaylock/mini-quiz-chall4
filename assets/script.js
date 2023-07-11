cf8fc412a9c1982ae19a4fb04168f61fa98de818
Alex Blaylock <129992589+AlexBlaylock@users.noreply.github.com>
2023-07-11 14:52:38 -0500

    added functions for display, answer, and end

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,50 @@ var startGameBtn = document.querySelector("#start-button")
 var quizBox = document.querySelector("#quiz-box")
 var quizQuestion = document.querySelector("#question-header")
 var userAnswer = document.querySelector("#user-answer")
-var submitButton = document.querySelector("#submit-answer")
+var submitBtn = document.querySelector("#submit-answer")
 
 // save game query selectors
-var quizOver = document.querySelector("#quiz-over-box")
+var quizOverBox = document.querySelector("#quiz-over-box")
 var initialLabel = document.querySelector("#initialsLabel")
-var quizOver = document.querySelector("#start-button")
+var saveQuizBtn = document.querySelector("#save-quiz")
 
-// button listeners
-startGameBtn.addEventListener
+// button event listeners
+startGameBtn.addEventListener("click", quizStart);
+submitBtn.addEventListener("click", userAnswer);
+saveQuizBtn.addEventListener("click", save-quiz);
+
+
+// start quiz function
+function startQuiz() {
+displayQuestion();
+startTime();
+}
+
+function displayQuestion() { 
+    let currentQ = questions[currentQuestionIndex];
+    questionEl.textContent = currentQ.question;
+}
+
+
+function answerQuestion() {
+    let currentQ = questions[currentQIndex]
+    let userAnswer = answerInput.value.trim().toLowerCase();
+
+    if (userAnswer === currentQ.answer.toLowerCase()) {
+        currentQuestionIndex++;
+        if (currentQ < questions.length) {
+            displayQuestion();
+        } else {
+            endQuiz();
+        }
+    } else {
+        time -= 30;
+    }
+    answerInput.value = ""
+}
+
+function endQuiz() {
+    clearInterval(timerId);
+    quixBox.style.disply = "none";    
+    quizOverBox.style.display = "block";
+}
